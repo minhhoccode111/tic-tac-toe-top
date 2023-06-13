9cb29cf8a95766dece52c3e6b90edb821e6b8f43
minhhoccode111 <minhhoccode111@gmail.com>
2023-06-13 23:59:33 +0700

    Commit before working with Minimax algorithm

--- new.js
+++ new.js
@@ -1,27 +1,61 @@
 // console.log("Hello world from new.js!");
+
+//#################### PLAYER ####################
 function Player(mark, bot) {
   let _bot = bot;
-  const getMark = () => mark;
+  let _mark = mark;
+  const getMark = () => _mark;
+  const setMark = (value) => (_mark = value);
   const isBot = () => bot;
   const toBot = (boolean) => (_bot = boolean);
   return {
     getMark,
+    setMark,
     isBot,
     toBot,
   };
 }
 
+//#################### DISPLAY ####################
+const display = (() => {
+  const dp = (board) => {
+    console.log(board);
+  };
+
+  const winner = (mark) => console.log(`${mark.toUpperCase()} is the winner!`);
+
+  const tie = () => console.log(`It's a tie game!`);
+
+  const invalid = (mark) =>
+    console.log(`${mark.toUpperCase()} just made an invalid move! Move again!`);
+
+  return {
+    invalid,
+    winner,
+    dp,
+    tie,
+  };
+})();
+
+//#################### AI ####################
 const ai = (() => {
   let _mode = 1; //see 1,3,5,9 moves ahead using minimax algorithm
 
   const setMode = (mode) => (_mode = mode);
-  const minimax = () => {};
+
+  const deepCopy = () => JSON.parse(JSON.stringify(board.getGrid()));
+
+  const available = (grid) => grid.filter((spot) => spot != "x" && spot != "o");
+
+  function minimax() {}
   return {
     setMode,
     minimax,
+    available,
   };
 })();
 
+//#################### BOARD ####################
 const board = (() => {
   let _grid = Array(9);
   const winnable = [
@@ -42,16 +76,14 @@ const board = (() => {
 
   const getGrid = () => _grid;
 
-  const setGrid = (i, mark) => {
-    if ((_grid[i] != "x") & (_grid[i] != "o")) _grid[i] = mark;
-  };
+  const isValid = (i) => _grid[i] !== "x" && _grid[i] !== "o";
+
+  const setGrid = (i, mark) => (_grid[i] = mark);
 
   const isTie = () => _grid.every((spot) => spot === "x" || spot === "o");
 
   const isWin = (mark) =>
     winnable.some((wins) => wins.every((win) => _grid[win] === mark));
-
-  const available = () => _grid.filter((spot) => spot != "x" && spot != "o");
 
   const reset = () => {
     for (let i = 0; i < 9; i++) {
@@ -60,15 +92,16 @@ const board = (() => {
   };
 
   return {
-    reset,
     getGrid,
+    isValid,
     setGrid,
-    available,
+    reset,
     isTie,
     isWin,
   };
 })();
 
+//#################### CONTROLLER ####################
 const controller = (() => {
   const player0 = Player("x", false);
   const player1 = Player("o", true);
@@ -79,7 +112,68 @@ const controller = (() => {
   const switchCurrent = () =>
     current === player0 ? (current = player1) : (current = player0);
 
-  // const setNumPlayer=num=>
+  const switchVs = (num) => {
+    if (num === 1) player1.toBot(true);
+    if (num === 2) player1.toBot(false);
+  };
 
-  return {};
+  const switchMark = () =>
+    player0.getMark() === "x" ? player0.setMark("o") : player1.setMark("x");
+
+  const aiPlayRound = (mark) => {
+    // const move = ai.minimax();
+
+    const avail = ai.available(board.getGrid());
+    const move = avail[Math.floor(Math.random() * avail.length)];
+    playRound(move, mark);
+  };
+
+  const playRound = (i, mark) => {
+    if (gameEnded) return;
+
+    if (!board.isValid(i)) return display.invalid(mark);
+
+    board.setGrid(i, mark);
+    display.dp(board.getGrid());
+
+    if (board.isWin(mark)) {
+      display.winner(mark);
+      gameEnded = true;
+      return;
+    }
+
+    if (board.isTie()) {
+      display.tie();
+      gameEnded = true;
+      return;
+    }
+
+    switchCurrent();
+
+    if (current.isBot()) aiPlayRound(current.getMark());
+  };
+
+  const reset = () => {
+    gameEnded = true;
+    current = player0;
+  };
+  return {
+    switchCurrent,
+    switchMark,
+    switchVs,
+    reset,
+    playRound,
+    current,
+  };
+})();
+
+//#################### LISTENER ####################
+const listener = (() => {
+  window.addEventListener("keyup", (e) => {
+    const arr = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    console.log(e.key);
+    if (arr.includes(e.key)) {
+      controller.playRound(arr.indexOf(e.key), controller.current.getMark());
+    }
+  });
 })();
